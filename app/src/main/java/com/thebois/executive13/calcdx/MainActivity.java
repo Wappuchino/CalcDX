c4b0adc45239552e81a0d0bec9dde84dbb574726
Arami Guerra de la Llera <arami.g@outlook.com>
2017-12-15 21:14:36 -0600

    starting the project

---
+++ app/src/main/java/com/thebois/executive13/calcdx/MainActivity.java
@@ -0,0 +1,13 @@
+package com.thebois.executive13.calcdx;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
