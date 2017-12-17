81309ccd4cba3ae736167a5fff1f97fde9f4fe14
Wappuchino <wappuchino@gmail.com>
2017-12-17 17:23:21 -0600

    Base Interfaces 2

---
+++ app/src/main/java/com/thebois/wappuchino/calcdx/solver/IParser.java
@@ -0,0 +1,32 @@
+/*
+ ***************************************************************************************************
+ * The IParser represents any expression parser. It provides access the the parsing functionality  *
+ * of the parser.                                                                                  *
+ *                                                                                                 *
+ * Note that parsers only return the first term of the expression that would be evaluated using    *
+ * stamdard order of operations. However, keep in mind that the terms form a parse tree which      *
+ * makes the first term also the root of the parse tree, meaning that all terms are accessible     *
+ * through the first (also the root) term.                                                         *
+ *                                                                                                 *
+ * - Wappuchino                                                                                    *
+ **************************************************************************************************/
+
+package com.thebois.wappuchino.calcdx.solver;
+
+/**
+ * The IParser interface exposes the parsing functionality of a parser.
+ * It also represents the base type for all expression parsers.
+ */
+interface IParser
+{
+    /**
+     * The parse function of the parser. It accepts a string the format accepted by the parser
+     * and attempts to parse the string into an expression. Should the string be improperly
+     * formatted, the parser will return null; otherwise, it returns the first term (as in the
+     * first term that should happen when order of operations is applied)
+     *
+     * @param expression A single formatted string representing a mathematical expression
+     * @return The first term that would be evaluated in standard order of operations
+     */
+    ITerm parse(String expression);
+}
--- app/src/main/java/com/thebois/wappuchino/calcdx/solver/ITerm.java
+++ app/src/main/java/com/thebois/wappuchino/calcdx/solver/ITerm.java
@@ -1,8 +1,56 @@
-
+/*
+ ***************************************************************************************************
+ * The ITerm is a base type that represents any term that could appear in a mathematical           *
+ * expression.                                                                                     *
+ *                                                                                                 *
+ * The term provides access to functions that evaluate the term numerically                        *
+ * and return a string representation of the term.                                                 *
+ *                                                                                                 *
+ * The terms also form a parse tree as the terms can have children terms which (in most cases)     *
+ * represent operands and the like. Note that the first term of an expression is always the root   *
+ * of the parse tree. Access to children is exposed through this interface.                        *
+ *                                                                                                 *
+ * - Wappuchino                                                                                    *
+ **************************************************************************************************/
 
 package com.thebois.wappuchino.calcdx.solver;
 
+import java.math.BigDecimal;
+
+/**
+ * The ITerm interface which exposes functionality for making a string representation of a term,
+ * and evaluating a term numerically.
+ */
 interface ITerm
 {
+    /**
+     * Retrieves the string representation of the term.
+     *
+     * This is the same as the toString function that is seen with all Java objects; however,
+     * the method is declared here again in order to force a contract with all derived classes
+     * to implement functionality for making a string representation as all terms should have
+     * a string representation.
+     *
+     * @return A string representation of the term.
+     */
+    String toString();
 
+    /**
+     * Evaluates a term numerically and then returns the result.
+     *
+     * @param precision The number of digits of precision that should be used in all calculations
+     * @return A BigDecimal object representing the result of evaluating the term numerically
+     */
+    BigDecimal evaluate(int precision);
+
+    /**
+     * Gets a view children of the term. Modifications to this view are neither guaranteed to modify
+     * or not modify the children of the term.
+     *
+     * In most cases, the children of the term denote the operands of the term if it is an
+     * operation term. However, soem terms may have no children or a variable number of children.
+     *
+     * @return A view of the children of the term
+     */
+    ITerm[] getChildren();
 }
